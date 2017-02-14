a9455e4545841cff8b23c9f28fd7a1eb6dea88e6
est31 <MTest31@outlook.com>
2017-02-14 11:09:34 +0100

    Use libc and log crates from crates.io instead through a feature

--- lib.rs
+++ lib.rs
@@ -7,7 +7,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![feature(alloc, libc)]
+#![feature(alloc)]
 
 extern crate alloc;
 extern crate byteorder;
